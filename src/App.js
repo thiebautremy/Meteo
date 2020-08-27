7d8867e887451ae7f790caddb2c383b421ce74d6
thiebautremy <59329427+thiebautremy@users.noreply.github.com>
2020-08-27 16:21:39 +0200

    correction proptypes + ajout spinner avant loading data from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import './App.scss';
 import WeatherWidget from './COMPONENTS/WeatherWidget/WeatherWidget';
 
 function App() {
-  console.log(process.env);
   return (
     <div className="App">
       <WeatherWidget />
--- src/COMPONENTS/WeatherWidget/WeatherWidget.js
+++ src/COMPONENTS/WeatherWidget/WeatherWidget.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import Spinner from '../Spinner/spinner';
 
 import './style.css';
 
 const WeatherWidget = () => {
   const [temp, setTemp] = useState(0);
-  const [city, setCity] = useState('Nancy');
+  const [city] = useState('Lima');
   const [coord, setCoord] = useState(0);
   const [weather, setWeather] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_BASE_URL}?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
@@ -16,27 +18,32 @@ const WeatherWidget = () => {
       setTemp(Math.round(res.data.main.temp))
       setCoord(res.data.coord);
       setWeather(res.data.weather[0].main);
+      setLoading(false);
     })
     .catch((error) => console.log(error))
   }, [city]);
   return(
     <div className="weatherwidget">
-    <p className="weatherwidget__city">{city}</p>
-    <p className="weatherwidget__coord">Lat : {coord.lat}</p>
-    <p className="weatherwidget__coord">Lon : {coord.lon}</p>
-    <p className="weatherwidget__weather">{weather}</p>
-    <p className="weatherwidget__temp">{temp} ° C</p>
-  </div>
-  
-);
+      {loading && <Spinner />}
+      {!loading && 
+        <>
+          <p className="weatherwidget__city">{city}</p>
+          <p className="weatherwidget__coord">Lat : {coord.lat}</p>
+          <p className="weatherwidget__coord">Lon : {coord.lon}</p>
+          <p className="weatherwidget__weather">{weather}</p>
+          <p className="weatherwidget__temp">{temp} ° C</p>
+        </>
+      }
+    </div>
+  );
 };
 
 
 WeatherWidget.propTypes = {
-  city: PropTypes.string.isRequired,
-  temp: PropTypes.number.isRequired,
-  coord: PropTypes.number.isRequired,
-  weather: PropTypes.string.isRequired,
+  city: PropTypes.string,
+  temp: PropTypes.number,
+  coord: PropTypes.number,
+  weather: PropTypes.string,
 };
 
 export default WeatherWidget;
